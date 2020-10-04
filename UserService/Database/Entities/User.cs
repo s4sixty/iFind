c6e55d7077dccff87c1bd68638d5028556303532
Samir <samirq2@outlook.com>
2020-10-04 09:30:46 +0200

    User form validation

--- UserService/Database/Entities/User.cs
+++ UserService/Database/Entities/User.cs
@@ -10,12 +10,17 @@ namespace UserService.Database.Entities
     public class User
     {
         public int Id { get; set; }
+        [StringLength(20, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 3)]
+        [Required]
         public string firstName { get; set; }
+        [StringLength(20, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 3)]
+        [Required]
         public string lastName { get; set; }
         [Required]
         [EmailAddress]
         public string email { get; set; }
         [Required]
+        [MinLength(6)]
         public string password { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
---
+++ UserService/Migrations/20201003114009_validations.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserService.Migrations
+{
+    public partial class validations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "Users",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
