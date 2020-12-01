0100fad4069aefb4eedf62da2f1c74c13064abe2
Samir <samirq2@outlook.com>
2020-12-01 11:31:13 +0100

    found items DTO

--- FoundItemsService/Database/Entities/FoundItem.cs
+++ FoundItemsService/Database/Entities/FoundItem.cs
@@ -33,19 +33,21 @@ namespace FoundItemsService.Database.Entities
 
     public class FoundItemDTO
     {
+        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         public string Model { get; set; }
         [Required]
-        public DateTime LostAt { get; set; }
+        public DateTime FoundAt { get; set; }
         [StringLength(20, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 2)]
         [Required]
         public string Color { get; set; }
         [StringLength(5000, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 0)]
         public string Description { get; set; }
-        public bool Found { get; set; }
-        public DateTime? FoundAt { get; set; }
+        public string city { get; set; }
+        public bool FoundOwner { get; set; }
+        public DateTime? FoundOwnerAt { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime ModifiedAt { get; set; }
+        public string Photo { get; set; }
     }
 }
