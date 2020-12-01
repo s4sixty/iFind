9da99db294e22110f5328cb7c37d87d97d36192b
Samir <samirq2@outlook.com>
2020-12-01 11:08:53 +0100

    changes

--- FoundItemsService/Database/Entities/Comments.cs
+++ FoundItemsService/Database/Entities/Comments.cs
@@ -9,6 +9,7 @@ namespace FoundItemsService.Database.Entities
     {
         public int Id { get; set; }
         public int UserId { get; set; }
+        [Required]
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
         public FoundItem FoundItem { get; set; }
